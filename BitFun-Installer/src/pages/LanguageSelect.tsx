03fbc212eec44982e4f289f27c58bc61931513cf
kev1n77 <fmy077@outlook.com>
2026-04-13 11:45:55 +0800

    chore: bump version to 0.2.5

--- BitFun-Installer/src/pages/LanguageSelect.tsx
+++ BitFun-Installer/src/pages/LanguageSelect.tsx
@@ -86,7 +86,7 @@ export function LanguageSelect({ onSelect }: LanguageSelectProps) {
             opacity: 0.6,
             letterSpacing: '0.5px',
           }}>
-            <div>Version 0.2.2</div>
+            <div>Version 0.2.5</div>
             <div style={{
               marginTop: 6,
               lineHeight: 1.5,
